232bcee0138880d73af0b50d83ec566d7cac4acd
andres <andresfelix951@gmail.com>
2023-09-08 13:27:21 -0500

    adding heroku deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,36 @@
 plugins {
-	java
-	id("org.springframework.boot") version "2.7.15"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    java
+    id("org.springframework.boot") version "2.7.15"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "com.platzi"
-version = "0.0.1-SNAPSHOT"
+version = "1.0"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
-	runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:postgresql")
 
-	implementation ("org.mapstruct:mapstruct:1.5.5.Final")
-	annotationProcessor ("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    implementation("org.mapstruct:mapstruct:1.3.1.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.3.1.Final")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
